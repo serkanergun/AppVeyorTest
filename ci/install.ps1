4df483bb3ea39f386441e38fa6001c7c69c83e4d
serkanergun <serkan.ergun@gmail.com>
2015-12-03 15:04:58 +0200

    Eigen3 added

--- ci/install.ps1
+++ ci/install.ps1
@@ -1,3 +1,24 @@
+function install_msi($folder, $name, $url)
+{
+  If (!(Test-Path "$folder\$name.msi")){
+	Write-Host "Downloading $name"
+	(new-object net.webclient).DownloadFile($url, "$folder\$name.msi")
+  }
+  Write-Host "Installing $name"
+  Start-Process -FilePath "msiexec.exe" -ArgumentList "/quiet /norestart /qn /L*V! $name.log /i $name.msi" -Wait
+}
+
+function install_zip($folder, $name, $url, $dir)
+{
+  If (!(Test-Path "$folder\$name.zip")){
+	Write-Host "Downloading $name"
+	(new-object net.webclient).DownloadFile($url, "$folder\$name.zip")
+    
+	Write-Host "Extracting $name"
+    Start-Process -FilePath "7z.exe" -ArgumentList "x $name.zip -o$folder\$dir -y" -Wait
+  }
+}
+
 function main()
 {
   try 
@@ -5,23 +26,28 @@ function main()
     $depFolder = "c:\deps"
 
     $depends = @{
-      "cppcheck" = "http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.71/cppcheck-1.71-x64-Setup.msi?r=&ts=1449135605&use_mirror=skylink"	
+      "cppcheck" = @("msi", "http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.71/cppcheck-1.71-x64-Setup.msi?r=&ts=1449135605&use_mirror=skylink")
+	  "Eigen" = @("zip", "http://bitbucket.org/eigen/eigen/get/3.2.7.zip", ".")
     }
 
     $old_dir = $pwd
 
-	If (!(Test-Path -Path $depFolder)){
-	  Write-Host "Creating Folder: $depFolder"
-	  New-Item -ItemType directory -Path $depFolder | Out-Null
-	}
+    If (!(Test-Path -Path $depFolder)){
+      Write-Host "Creating Folder: $depFolder"
+      New-Item -ItemType directory -Path $depFolder | Out-Null
+    }
     cd $depFolder
     foreach ($depend in $depends.GetEnumerator()) {
-      If (!(Test-Path "$depFolder\$($depend.Name).msi")){
-	    Write-Host "Downloading $($depend.Name)"
-        (new-object net.webclient).DownloadFile($($depend.Value), "$depFolder\$($depend.Name).msi")
+      $name = $($depend.Name)
+      $vals = $($depend.Value)
+      switch ($vals[0]) {
+        "msi" {
+          install_msi $depFolder $name $vals[1] 
+        }
+		"zip" {
+		  install_zip $depFolder $name $vals[1] $vals[2]
+		}
       }
-      Write-Host "Installing $($depend.Name)"
-      Start-Process -FilePath "msiexec.exe" -ArgumentList "/quiet /norestart /qn /L*V! $($depend.Name).log /i $($depend.Name).msi" -Wait
     }
 
     cd $old_dir
