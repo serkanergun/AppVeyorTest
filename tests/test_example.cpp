cafc8d2faede6375e51c866109c1da683838daaa
serkanergun <serkan.ergun@gmail.com>
2015-12-03 09:48:14 +0200

    * Test Pass

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -11,6 +11,6 @@ BOOST_AUTO_TEST_CASE( text_test )
 {
     std::string text1 = "text1";
     std::string text2 = "text2";
-    BOOST_CHECK_NE( text1, text1 );
+    BOOST_CHECK_NE( text1, text2 );
 }
 
