74e7a384564f794954e2fc5f0cdb64abf2de583f
serkanergun <serkan.ergun@gmail.com>
2015-12-03 12:31:24 +0200

    * Appveyor tests

--- ci/install.ps1
+++ ci/install.ps1
@@ -1,25 +1,27 @@
-function ExecuteCommand($command, $command_args)
-{
-  Write-Host $command $command_args
-  Start-Process -FilePath $command -ArgumentList $command_args -Wait
-}
-
-function InstallPrerequisites($uri, $msi)
-{
-  (new-object net.webclient).DownloadFile($uri, "c:\projects\$msi")
-
-  $install_command = "msiexec.exe"
-  $install_args    = "/quiet /norestart /qn /L*V! log.txt /i $msi"
-  ExecuteCommand $install_command $install_args
-}
-
 function main()
 {
   try 
   {
+    $depFolder = "c:\deps"
+
+    $depends = @{
+      "cppcheck" = "http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.71/cppcheck-1.71-x64-Setup.msi?r=&ts=1449135605&use_mirror=skylink"	
+    }
+
     $old_dir = $pwd
-    cd "C:/projects"
-    InstallPrerequisites "http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.71/cppcheck-1.71-x64-Setup.msi?r=&ts=1449135605&use_mirror=skylink" "cppcheck.msi" 
+
+	If (!(Test-Path -Path $depFolder)){
+	  New-Item -ItemType directory -Path $depFolder | Out-Null
+	}
+    cd $depFolder
+    foreach ($depend in $depends.GetEnumerator()) {
+      If (!(Test-Path "$depFolder\$($depend.Name).msi")){
+        (new-object net.webclient).DownloadFile($($depend.Value), "$depFolder\$($depend.Name).msi")
+      }
+      Write-Host "Installing $($depend.Name)"
+      Start-Process -FilePath "msiexec.exe" -ArgumentList "/quiet /norestart /qn /L*V! $($depend.Name).log /i $($depend.Name).msi" -Wait
+    }
+
     cd $old_dir
   }
   catch
